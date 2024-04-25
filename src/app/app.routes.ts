2b3874dde9ab9af6cdb8f27ab3c413f9f06d89c8
Miti <mitisuaki@gmail.com>
2024-04-25 03:04:48 -0300

    side navbar done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
-  {path:"home", component: HomePageComponent},
-  {path:"", redirectTo: "home", pathMatch: "full"}
+  {path:"", component: HomePageComponent}
 ];
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderWithoutNavComponent } from "../../components/headers/header-without-nav/header-without-nav.component";
 import { HeaderWithNavComponent } from "../../components/headers/header-with-nav/header-with-nav.component";
+import { SideNavbarComponent } from "../../components/bodies/side-navbar/side-navbar.component";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [HeaderWithoutNavComponent, HeaderWithNavComponent]
+    imports: [HeaderWithoutNavComponent, HeaderWithNavComponent, SideNavbarComponent]
 })
 export class HomePageComponent {
 
