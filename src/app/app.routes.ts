8792bfdedf138ac920cf8882081c5ab3e2567ec1
Miti <mitisuaki@gmail.com>
2024-04-28 01:47:41 -0300

    second home-page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomePage2Component } from './pages/home-page-2/home-page-2.component';
 
 export const routes: Routes = [
-  {path:"", component: HomePageComponent}
+  {path:"", component: HomePageComponent},
+  {path:"home-2", component: HomePage2Component}
 ];
